fdd32b52188c236478f12673e8957da0f51178b6
Brushshshshs <90699741+Brushshshshs@users.noreply.github.com>
2024-10-26 21:33:17 +0300

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -5,7 +5,7 @@ while ind < len(my_list):
     if my_list[ind] == 0:
         ind += 1
         continue
-    if my_list[ind] >= 0:
+    elif my_list[ind] >= 0:
             print (my_list[ind])
             ind += 1
     else:
