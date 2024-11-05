8c123eb09c1623122794d0e5f40467a33a760f31
Brushshshshs <90699741+Brushshshshs@users.noreply.github.com>
2024-11-05 23:27:09 +0300

    Update 3.5.py

--- 3.5.py
+++ 3.5.py
@@ -1,13 +1,20 @@
 def get_multiplied_digits(number):
+    number = int(number)
     first = []
     str_number = str(number)
-
+    n = 0
     f = first.append(str_number[0])
     f = int(first[0])
+    print(f)
     if len(str_number) > 1:
-        return (f * get_multiplied_digits(int(str_number[1:])))
+        if str_number[1:] != '0':
+            return (f * get_multiplied_digits(int(str_number[1:])))
+        else:
+            return f
     else:
-        return (f)
+        return f
+
 
 print(get_multiplied_digits(40203))
-print(get_multiplied_digits(4))
+print(get_multiplied_digits(40))
+print(get_multiplied_digits(1))
